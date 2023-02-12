18f9995fcbae9b4fe47f8766c9b2a9150efc3772
ryudb0 <ryudb030@gmail.com>
2023-02-12 22:35:17 +0900

    Random 클래스 사용

---
+++ src/Ex9_27.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Ex9_27 {
+	public static void main(String[] args) {
+		Random rand = new Random(1);
+		Random rand2= new Random(1);
+		
+		System.out.println("= rand =");
+		for(int i=0; i < 5; i++) {
+			System.out.println(i + ":" + rand.nextInt());
+		}
+		
+		System.out.println();
+		
+		System.out.println("= rand2 =");
+		for(int i=0; i<5; i++) {
+			System.out.println(i + ":"+rand2.nextInt());
+		}
+	}
+}
